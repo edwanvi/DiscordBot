e0a024a61ed319f415d073775249df8ab02c5df0
tkdberger <tkdberger@gmail.com>
2016-04-08 15:02:50 -0400

    Change commands from ! to ?

--- main.py
+++ main.py
@@ -8,24 +8,28 @@ async def on_ready():
     print('Logged in as')
     print(client.user.name)
     print(client.user.id)
-    print('------')
 
 @client.event
 async def on_message(message):
-    if message.content.startswith('!test'):
+    if message.content.startswith('?test'):
         counter = 0
         tmp = await client.send_message(message.channel, 'Calculating messages...')
         async for log in client.logs_from(message.channel, limit=100):
             if log.author == message.author:
                 counter += 1
-
         await client.edit_message(tmp, 'You have {} messages.'.format(counter))
-    elif message.content.startswith('!sleep'):
+
+    elif message.content.startswith('?sleep'):
         await asyncio.sleep(5)
         await client.send_message(message.channel, 'Done sleeping')
-    elif message.content.startswith('!robot'):
+
+    elif message.content.startswith('?robot'):
         await client.send_message(message.channel, 'BLEEP BLOOP')
-    elif message.content == '!cease':
+
+    elif message.content.startswith('?bug'):
+        await client.send_message(message.channel, "It's not a bug, it's a feature.")
+
+    elif message.content == '?cease':
         await client.send_message(message.channel, 'Authenticating...')
         author = message.author
         authorname = author.name
@@ -34,5 +38,9 @@ async def on_message(message):
             await client.logout()
         else:
             await client.send_message(message.channel, "Sorry, you don't have the ability to do that.")
+
+    elif message.content.startswith('?echo'):
+        echo = message.content[6:]
+        await client.send_message(message.channel, echo)
 
 client.run('tkdberger@gmail.com', 'full_leobitz')
