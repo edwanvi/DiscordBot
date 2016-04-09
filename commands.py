9755f96a7991424b4b54a7d14f28d777d7884602
tkdberger <tkdberger@gmail.com>
2016-04-09 14:38:59 -0400

    Fixed commands.py
    Thanks @confuseh

--- commands.py
+++ commands.py
@@ -4,17 +4,15 @@ I hope i can get this to work.
 """
 # Import the needed modules.
 import discord
-from discord.ext import commands
 import asyncio
 import extrautils
 
 # create a new Client for the bot to run on.
-client = discord.Client()
 # Open the file that contains the username and password for the account.
 username = extrautils.getCreds('creds.txt', 'username')
 password = extrautils.getCreds('creds.txt', 'password')
 
 
-async def send_help(message):
+async def send_help(client, message):
     print("function was called")
     await client.send_message(message.channel, "Sorry, you don't have the ability to do that.")
--- main.py
+++ main.py
@@ -57,6 +57,6 @@ async def on_message(message):
 
     # run commands from commands.py
     elif message.content.startswith("?help"):
-        await commands.send_help(message)
+        await commands.send_help(client, message)
 
 client.run(username, password)
