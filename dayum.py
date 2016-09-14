e83ff5b8bae9b2ba8caaef7d7a52d915ea77f685
Edwan Vi <tkdberger@gmail.com>
2016-09-14 15:34:13 -0400

    Removed a stupid joke

--- dayum.py
+++ dayum.py
@@ -1,17 +1,15 @@
 # import discord
 import json
-
+gitxt = ' so https://www.github.com/'
 
 def readTheJson(filename):
     # read it
     with open(filename) as data_file:
         data = json.load(data_file)
-        print(data)
+        #print(data) <- just don't
         print(data["magi-bot"])
-
 
 sources = {'self': 'tkdberger/DiscordBot', 'rdanny': 'Rapptz/RoboDanny'}
 print(sources['self'] + ' so https://www.github.com/' + sources['self'])
-print(sources['rdanny'])
+print(sources['rdanny'] + gitxt + sources['rdanny'])
 readTheJson('data.json')
-
