814c14f5572df1435d999bc692f587ca4e778916
Edwan Vi <tkdberger@gmail.com>
2016-10-14 16:37:29 -0400

    Remove obselete auto-committer

--- main.py
+++ main.py
@@ -57,9 +57,6 @@ async def on_message(message):
         authorname = author.id
         if authorname == '164342765394591744':
             await client.send_message(message.channel, 'Ceasing.')
-            print("Adding and committing database.json")
-            call("git add database.json")
-            call('''git commit -m "AUTOMATED COMMIT OF database.json"''')
             await client.logout()
         else:
             await client.send_message(message.channel, "Sorry, you don't have the ability to do that.")
